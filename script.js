5f1450f077b683dfc1aed2e56b89d155ddd167d0
volotovskiy <2rva@bk.ru>
2024-07-01 00:03:37 +1000

    fix validate\save\showError

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let maxValue = 100;
 //maxValue = maxValue > 999 ? 999  : maxValue;
 
 //alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
-let answerNumber  = Math.floor((minValue + maxValue) / 2);
+let answerNumber = Math.floor((minValue + maxValue) / 2);
 let orderNumber = 1;
 let gameRun = true;
 
@@ -16,7 +16,7 @@ const orderNumberField = document.getElementById('orderNumberField');
 const answerField = document.getElementById('answerField');
 
 orderNumberField.innerText = orderNumber;
-answerField.innerText = `Вы загадали число ${answerNumber }?`;
+answerField.innerText = `Вы загадали число ${answerNumber}?`;
 
 const check = [
     `Я всегда угадываю\n\u{1F60E}`,
@@ -39,7 +39,7 @@ const answerPhrase = [
 
 const progressWin = document.getElementById('progressWin');
 const progressLose = document.getElementById('progressLose');
-let progress = [[],[]];  // Прогресс удач и нет 
+let progress = [[], []];  // Прогресс удач и нет 
 let win = 0;
 let lose = 0;
 
@@ -50,28 +50,33 @@ document.getElementById('btnRetry').addEventListener('click', function () {
     orderNumberField.innerText = orderNumber;
     gameRun = true;
 
+    minValue = parseInt(document.getElementById('validationCustom01').value);
+    maxValue = parseInt(document.getElementById('validationServer02').value);
+
     answerNumber = Math.floor((minValue + maxValue) / 2);
     answerField.innerText = Questions[0] + `${answerNumber}?`;
 })
 
 document.getElementById('btnOver').addEventListener('click', function () {
-    if (gameRun){
-        if (minValue === maxValue){
-            const phraseRandom = Math.floor(Math.random()*answerPhrase.length);
+    if (gameRun) {
+        if (minValue === maxValue) {
+            const phraseRandom = Math.floor(Math.random() * answerPhrase.length);
 
-                lose = lose +1;
-                progress[1][0] = lose;
-                progressLose.innerText = progress[1][0];
+            lose = lose + 1;
+            progress[1][0] = lose;
+            progressLose.innerText = progress[1][0];
             answerField.innerText = answerPhrase[phraseRandom];
             gameRun = false;
         } else {
-            minValue = answerNumber  + 1;
-            answerNumber  = Math.floor((minValue + maxValue) / 2);
+            minValue = answerNumber + 1;
+
+            answerNumber = Math.floor((minValue + maxValue) / 2);
+
             orderNumber++;
 
             orderNumberField.innerText = orderNumber;
-            
-            const phraseRandom = Math.floor(Math.random()*Questions.length);
+
+            const phraseRandom = Math.floor(Math.random() * Questions.length);
 
             const textNumber = numberToText(answerNumber);
             if (textNumber.length <= 20) {
@@ -85,24 +90,24 @@ document.getElementById('btnOver').addEventListener('click', function () {
 })
 
 document.getElementById('btnLess').addEventListener('click', function () {
-    if (gameRun){
-        if (minValue === maxValue){
-            const phraseRandom = Math.floor(Math.random()*answerPhrase.length);
-            
-            lose = lose +1;
+    if (gameRun) {
+        if (minValue === maxValue) {
+            const phraseRandom = Math.floor(Math.random() * answerPhrase.length);
+
+            lose = lose + 1;
             progress[1][0] = lose;
             progressLose.innerText = progress[1][0];
             answerField.innerText = answerPhrase[phraseRandom];
             gameRun = false;
         } else {
 
-            maxValue = Math.max(minValue, answerNumber - 1); 
-            answerNumber  = Math.floor((minValue + maxValue) / 2);
+            maxValue = Math.max(minValue, answerNumber - 1);
+            answerNumber = Math.floor((minValue + maxValue) / 2);
             orderNumber++;
 
             orderNumberField.innerText = orderNumber;
 
-            const phraseRandom = Math.floor(Math.random()*Questions.length);
+            const phraseRandom = Math.floor(Math.random() * Questions.length);
 
             const textNumber = numberToText(answerNumber);
             if (textNumber.length <= 20) {
@@ -110,7 +115,7 @@ document.getElementById('btnLess').addEventListener('click', function () {
             } else {
                 answerField.innerText = Questions[phraseRandom] + `${answerNumber}?`;
             }
-    
+
         }
     }
 })
@@ -118,13 +123,13 @@ document.getElementById('btnLess').addEventListener('click', function () {
 
 
 document.getElementById('btnEqual').addEventListener('click', function () {
-    if (gameRun){
+    if (gameRun) {
 
-        const phraseRandom = Math.floor(Math.random()*check.length);
+        const phraseRandom = Math.floor(Math.random() * check.length);
         answerField.innerText = check[phraseRandom];
-        
-        win = win +1;
-        progress[0][0]=win;
+
+        win = win + 1;
+        progress[0][0] = win;
         progressWin.innerText = progress[0][0];
 
         gameRun = false;
@@ -141,79 +146,171 @@ const form1 = document.querySelector('.needs-validation');
 
 $('#modalExample').modal('hide');
 
+// Функция для проверки значений
+function validateValues(minValue, maxValue) {
+    // Проверка на NaN
+    if (isNaN(minValue) || isNaN(maxValue)) {
+        showError("not_a_number");
+        return false; // Прерывание выполнения, если есть ошибка
+    }
+
+    // Проверка и коррекция минимального значения
+    let iminVal = false;
+
+    if ((minValue < -999 || maxValue > 999) || (maxValue < -999 || minValue > 999)) {
+        showError("invalid_greater");
+        iminVal = true;
+        return false; // Прерывание выполнения, если есть ошибка
+    }
+
+    if (minValue > maxValue) {
+        if (iminVal === false) {
+            showError("iminVal_maxVal");
+            iminVal = false;
+        }
+        return false; // Прерывание выполнения, если есть ошибка
+    }
+if (minValue == maxValue){
+    showError("equally");
+    return false; // Прерывание выполнения, если есть ошибка
+}
+    // Отображение значений на странице
+    document.getElementById('numbersMin').innerText = minValue;
+    document.getElementById('numbersMax').innerText = maxValue;
+
+    return true; // Значения прошли проверку
+}
+
+
 form1.addEventListener('submit', (event) => {
     event.preventDefault();
 
-     var forms = document.querySelectorAll('.needs-validation')
-  
-     // Зацикливайтесь на них и предотвращайте отправку
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Зацикливайтесь на них и предотвращайте отправку
     Array.prototype.slice.call(forms)
-       .forEach(function (form) {
-         form.addEventListener('submit', function (event)  {
-           if (!form.checkValidity()) {
-            bsOffcanvas.show();
-             event.preventDefault()
-             event.stopPropagation()
-           }
-          form.classList.add('was-validated')
-          if(form.checkValidity()){
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+                form.classList.add('was-validated')
 
-             minValue = parseInt(document.getElementById('validationCustom01').value) ;
-             maxValue = parseInt(document.getElementById('validationServer02').value) ;
+            }, false)
 
-            if (isNaN(minValue) || isNaN(maxValue)){
-                 const modal = new bootstrap.Modal(document.getElementById('modalExample'));
-                 modal.show();
-                 bsOffcanvas.show();
-                 document.getElementById('validationCustom01').value = parseInt(document.getElementById('validationCustom01').value) || 0;
-                document.getElementById('validationServer02').value = parseInt(document.getElementById('validationServer02').value) ||100;
-                
+            if (form.checkValidity()) {
+
+                minValue = parseInt(document.getElementById('validationCustom01').value);
+                maxValue = parseInt(document.getElementById('validationServer02').value);
+
+                // Вызов функции для проверки значений
+                if (validateValues(minValue, maxValue)) {
+                    // Включение кнопки "Играть"
+                    document.getElementById('btnGame').disabled = false;
+                } else document.getElementById('btnGame').disabled = true;
             }
-if (minValue  < -999 ){
-    // Проверка и коррекция минимального значения
-minValue = minValue < -999 ? -999 : minValue;
-}
-if (maxValue > 999){
-    maxValue = maxValue > 999 ? 999  : maxValue;
-}
-document.getElementById('numbersMin').innerText = minValue;
-document.getElementById('numbersMax').innerText = maxValue;  
-           }
-        }, false)
-       })
-
-  document.getElementById('btnGame').addEventListener('click', function () {
-    $('#staticBackdrop').offcanvas('hide');
-  })
-     
+        });
 });
 
+document.getElementById('btnGame').addEventListener('click', function () {
+  
+    minValue = parseInt(document.getElementById('validationCustom01').value);
+    maxValue = parseInt(document.getElementById('validationServer02').value);
 
+    // Вызов функции для проверки значений
+    if (validateValues(minValue, maxValue)) {
+
+        var toast = new bootstrap.Toast(document.getElementById('liveToast'));
+        toast.show();
+
+        answerNumber = Math.floor((minValue + maxValue) / 2);
+
+        const phraseRandom = Math.floor(Math.random() * Questions.length);
+        const textNumber = numberToText(answerNumber);
+   
+        if (textNumber.length <= 20) {
+            answerField.innerText = Questions[phraseRandom] + `${textNumber}?`;
+        } else {
+            answerField.innerText = Questions[phraseRandom] + `${answerNumber}?`;
+        }
+
+        // Скрыть offcanvas
+        $('#staticBackdrop').offcanvas('hide');
+    }
+});
 // Функция для преобразования числа в текст
 function numberToText(number) {
     if (number === 0) {
-      return "ноль";
+        return "ноль";
     }
-  
-    const numbers = [
-      "", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять", "десять",
-      "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать"
-    ];
-  
-    const tens = ["", "", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят", "девяносто"];
-  
-    if (number < 20) {
-      return numbers[number];
-    } else if (number < 100) {
-      return tens[Math.floor(number / 10)] + (number % 10 !== 0 ? " " + numbers[number % 10] : "");
-    } else {
-      return numbers[Math.floor(number / 100)] + " " +  "сто" + (number % 100 !== 0 ? " " + numberToText(number % 100) : "");
-    }
-  }
-  
-  document.getElementById('prog').addEventListener('click', function () {
 
-     win = 0;
-     progressLose.innerText = 0;
-     progressWin.innerText = 0;
+    const numbers = [
+        "", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять", "десять",
+        "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать"
+    ];
+
+    const tens = ["", "", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят", "девяносто"];
+
+    if (number < 20) {
+        return numbers[number];
+    } else if (number < 100) {
+        return tens[Math.floor(number / 10)] + (number % 10 !== 0 ? " " + numbers[number % 10] : "");
+    } else {
+        return (number / 100 === 1 ? "" : numbers[Math.floor(number / 100)]) + " " + "сто" + (number % 100 !== 0 ? " " + numberToText(number % 100) : "");
+    }
+
+}
+// del progres
+document.getElementById('prog').addEventListener('click', function () {
+
+    win = 0;
+    progressLose.innerText = 0;
+    progressWin.innerText = 0;
 });
+
+// Error select numbers
+function showError(errorType) {
+    let errorMessage = "";
+    switch (errorType) {
+        case "not_a_number":
+            errorMessage = "Вы ввели не число.";
+            minValue = parseInt(document.getElementById('validationCustom01').value) || 0;
+            maxValue = parseInt(document.getElementById('validationServer02').value) || 100;
+
+            document.getElementById('validationCustom01').value = minValue;
+            document.getElementById('validationServer02').value = maxValue;
+
+            document.getElementById("message").innerHTML = "В одном из полей введено не цифровое значение. Для вашего удобства, поменял значение на стандартное.";
+            break;
+        case "invalid_greater":
+            minValue = minValue < -999 ? -999 : minValue;
+            minValue = minValue > 999 ? 999 : minValue;
+
+            maxValue = maxValue < -999 ? -999 : maxValue;
+            maxValue = maxValue > 999 ? 999 : maxValue;
+
+            document.getElementById('validationCustom01').value = minValue;
+            document.getElementById('validationServer02').value = maxValue;
+            errorMessage = "Превышен диапазон вводимого числа минимально число может быть -999, максимальное 999.";
+            document.getElementById("message").innerHTML = "Для вашего удобства, применил максимально возможное число.";
+            break;
+        case "iminVal_maxVal":
+            errorMessage = "Минимально значение больше чем максимальное.";
+            document.getElementById("message").innerHTML = "Введите коректные данные.";
+            break;
+        case "equally": 
+        errorMessage = "Минимально значение и максимальное значение равны.";
+        document.getElementById("message").innerHTML = "Введите корректные данные.";  
+        break;
+
+        default:
+            errorMessage = "Произошла ошибка при обработке данных.";
+    }
+    const modal = new bootstrap.Modal(document.getElementById('modalExample'));
+    modal.show();
+    bsOffcanvas.show();
+
+    document.getElementById("error-message").innerHTML = "<strong>" + errorMessage + "</strong>";
+
+}
